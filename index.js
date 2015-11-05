1d2f984f9392527be909dcdaf85b0697d22009fd
Robin Janssens <robin.janssens@gmail.com>
2015-11-05 16:49:19 +0000

    Reformatted code

--- index.js
+++ index.js
@@ -12,80 +12,84 @@ var through = require('through');
 
 var PLUGIN_NAME = 'gulp-merge-json';
 
-module.exports = function(fileName, edit, startObj, endObj, exportModule) {
-  if ((startObj && typeof startObj !== 'object') || (endObj && typeof endObj !== 'object')) {
-    throw new gutil.PluginError(PLUGIN_NAME, PLUGIN_NAME + ': Invalid start and/or end object!');
-  }
-
-  var editFunc;
-
-  if (typeof edit === 'function') {
-    editFunc = edit;
-  } else if (typeof edit === 'object') {
-    editFunc = function(json) { return merge(json, edit); };
-  } else {
-    editFunc = function(json) { return json; };
-  }
-
-  var merged = startObj || {};
-  var firstFile = null;
-  var conflictingKeys = [];
-
-  function parseAndMerge(file) {
-    if (file.isNull()) {
-      return this.queue(file);
+module.exports = function (fileName, edit, startObj, endObj, exportModule) {
+    if ((startObj && typeof startObj !== 'object') || (endObj && typeof endObj !== 'object')) {
+        throw new gutil.PluginError(PLUGIN_NAME, PLUGIN_NAME + ': Invalid start and/or end object!');
     }
 
-    if (file.isStream()) {
-      return this.emit('error', new gutil.PluginError(PLUGIN_NAME, PLUGIN_NAME + ': Streaming not supported!'));
+    var editFunc;
+
+    if (typeof edit === 'function') {
+        editFunc = edit;
+    } else if (typeof edit === 'object') {
+        editFunc = function (json) {
+            return merge(json, edit);
+        };
+    } else {
+        editFunc = function (json) {
+            return json;
+        };
     }
 
-    if (!firstFile) {
-      firstFile = file;
+    var merged = startObj || {};
+    var firstFile = null;
+    var conflictingKeys = [];
+
+    function parseAndMerge(file) {
+        if (file.isNull()) {
+            return this.queue(file);
+        }
+
+        if (file.isStream()) {
+            return this.emit('error', new gutil.PluginError(PLUGIN_NAME, PLUGIN_NAME + ': Streaming not supported!'));
+        }
+
+        if (!firstFile) {
+            firstFile = file;
+        }
+
+        try {
+            merged = merge(
+                function (val1, val2, key) {
+                    conflictingKeys.push({
+                        'val1': val1,
+                        'val2': val2,
+                        'key': key,
+                        'filePath': file.path
+                    });
+                    // There is no way to tell which value should be used, so just use the first one
+                    return val1;
+                }, [merged, editFunc(JSON.parse(file.contents.toString('utf8')))]);
+        } catch (err) {
+            return this.emit('error', new gutil.PluginError(PLUGIN_NAME, err));
+        }
     }
 
-    try {
-      merged = merge(
-          function(val1, val2, key) {
-            conflictingKeys.push({
-              'val1': val1,
-              'val2': val2,
-              'key': key,
-              'filePath': file.path
-            });
-            // There is no way to tell which value should be used, so just use the first one
-            return val1;
-          }, [merged, editFunc(JSON.parse(file.contents.toString('utf8')))]);
-    } catch (err) {
-      return this.emit('error', new gutil.PluginError(PLUGIN_NAME, err));
-    }
-  }
+    function endStream() {
+        if (!firstFile) {
+            return this.emit('end');
+        }
 
-  function endStream() {
-    if (!firstFile) {
-      return this.emit('end');
-    }
+        if (endObj) {
+            merged = merge(merged, endObj);
+        }
 
-    if (endObj) {
-      merged = merge(merged, endObj);
-    }
+        var contents = JSON.stringify(merged, null, '\t');
 
-    var contents = JSON.stringify(merged, null, '\t');
+        if (exportModule) {
+            contents = 'module.exports = ' + contents + ';';
+        }
 
-    if (exportModule) {
-      contents = 'module.exports = ' + contents + ';';
+        var output = new gutil.File({
+            cwd: firstFile.cwd,
+            base: firstFile.base,
+            path: path.join(firstFile.base, fileName),
+            contents: new Buffer(contents),
+        });
+
+        this.emit('data', output);
+        this.emit('end');
     }
 
-    var output = new gutil.File({
-      cwd: firstFile.cwd,
-      base: firstFile.base,
-      path: path.join(firstFile.base, fileName),
-      contents: new Buffer(contents),
-    });
-
-    this.emit('data', output);
-    this.emit('end');
-  }
-
-  return through(parseAndMerge, endStream);
+    return through(parseAndMerge, endStream);
 };
